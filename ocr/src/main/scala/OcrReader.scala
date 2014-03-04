78afdd67282a25b084e9832682a1c75bc3b4c298
russell <russellwstanley@gmail.com>
2014-03-04 20:51:48 +0800

    parsenumber passing

--- ocr/src/main/scala/OcrReader.scala
+++ ocr/src/main/scala/OcrReader.scala
@@ -1,8 +1,41 @@
 import java.io.File
+import scala.io.Source
 
 /**
  * Created by russell on 04/03/14.
  */
 class OcrReader {
-  def parse(input : File): Int = 0
+
+  val one = "     |  |"
+  val two = " _  || "
+  val three = " _  _| _|"
+  val four = "   |_|  |"
+  val five = " _ |_  _|"
+  val six = " _ |_ |_|"
+  val seven = " _   |  |"
+  val eight = " _ |_||_|"
+  val nine = " _ |_| _|"
+
+
+  def parse(source : Source): List[Int] = {
+    source.getLines().map(str => str.toList).grouped(4).map(fourLines => {
+      println(fourLines)
+    })
+    List(0)
+  }
+
+
+  def parseNumber(numberString : String) = numberString match {
+    case "     |  |" => 1
+    case " _  || " => 2
+    case " _  _| _|" => 3
+    case "   |_|  |"=> 4
+    case " _ |_  _|" => 5
+    case " _ |_ |_|" => 6
+    case " _   |  |" => 7
+    case " _ |_||_|" => 8
+    case " _ |_| _|" => 9
+  }
+
+
 }
--- ocr/src/test/scala/OcrReaderSpec.scala
+++ ocr/src/test/scala/OcrReaderSpec.scala
@@ -1,5 +1,6 @@
 import java.io.File
 import org.scalatest.{Matchers, FlatSpec}
+import scala.io.Source
 
 /**
  * Created by russell on 04/03/14.
@@ -7,8 +8,34 @@ import org.scalatest.{Matchers, FlatSpec}
 class OcrReaderSpec extends FlatSpec with Matchers{
   "An Ocr reader" should "parse a file correctly" in {
     val reader = new OcrReader()
-    val file = new File(getClass().getResource("/testfile1").getFile())
-    file.exists() should be (true)
-    reader.parse(file) should be (123456789)
+    val source = Source.fromFile(getClass().getResource("/testfile1").getFile())
+    reader.parse(source) should be (List(123456789))
   }
+
+  val one = "     |  |"
+  val two = " _  || "
+  val three = " _  _| _|"
+  val four = "   |_|  |"
+  val five = " _ |_  _|"
+  val six = " _ |_ |_|"
+  val seven = " _   |  |"
+  val eight = " _ |_||_|"
+  val nine = " _ |_| _|"
+
+  it should "parse individual number strings directly" in {
+    val reader = new OcrReader()
+    reader.parseNumber(one) should be (1)
+    reader.parseNumber(two) should be (2)
+    reader.parseNumber(three) should be (3)
+    reader.parseNumber(four) should be (4)
+    reader.parseNumber(five) should be (5)
+    reader.parseNumber(six) should be (6)
+    reader.parseNumber(seven) should be (7)
+    reader.parseNumber(eight) should be (8)
+    reader.parseNumber(nine) should be (9)
+
+
+  }
+
+
 }
